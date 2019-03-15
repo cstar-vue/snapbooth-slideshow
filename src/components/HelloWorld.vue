346764b81429af7819bcad3ef4ac8cac69525f74
starengineer <mrg8406@gmail.com>
2019-03-15 03:58:15 -0400

    add vue-flux slider

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <vue-flux
+      :options="fluxOptions"
+      :images="fluxImages"
+      :transitions="fluxTransitions"
+      ref="slider">
+    </vue-flux>
+
+    <button @click="$refs.slider.showImage('next')">NEXT</button>
   </div>
 </template>
 
 <script>
+import { VueFlux, Transitions } from 'vue-flux';
+// import 'vue-flux/dist/vue-flux.css';
+
 export default {
-  name: 'HelloWorld',
+  name: 'Snapbooth-Slider',
   props: {
-    msg: String
-  }
+  },
+
+  components: {
+      'vue-flux': VueFlux
+  },
+
+  data: () => ({
+      fluxOptions: {
+         autoplay: true
+      },
+      fluxImages: [
+         'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/baboon.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/barbara.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/cat.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/fruits.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/frymire.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/girl.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/goldhill.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/monarch.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/mountain.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/peppers.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/pool.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/serrano.png',
+         'https://homepages.cae.wisc.edu/~ece533/images/tulips.png',
+      ],
+      fluxTransitions: {
+            transitionTurn3d: Transitions.transitionTurn3d
+      }
+  })
 }
 </script>
 
@@ -54,5 +67,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.main {
+  height: 100vh;
 }
 </style>
